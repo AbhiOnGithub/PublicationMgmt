b0a35dc8bb15460370561115b26582abbf628c8c
Abhishek Kumar Gupta <Abhishek.Kumar.Gupta1@citrix.com>
2021-05-05 18:30:49 +0530

    structuring

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type App struct {
+}
+
+func (app *App) Run() error {
+	fmt.Println("Setting up our App")
+	return nil
+}
+
 func main() {
 	fmt.Printf(("Publication Mgmt"))
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our REST API")
+		fmt.Println(err)
+	}
 }
